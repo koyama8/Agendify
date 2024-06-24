0aca8e01efc37568d90b0fcc93704fc36a7e413f
Matheus Koyama <koyamaCRS@gmail.com>
2024-06-24 16:56:41 -0300

    - Ajustando as rotas e navegação com orouter-outlet - Rota de contatos e Formulários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from './componentes/container/container.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { SeparadorComponent } from './componentes/separador/separador.component';
-import { ContatoComponent } from './componentes/contato/contato.component';
-import { FormsModule } from '@angular/forms';
-import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
-
-interface Contato {
-  id: number;
-  nome: string;
-  telefone: string;
-}
-
-import agenda from './agenda.json'
+import { FormularioContatoComponent } from "./paginas/formulario-contato/formulario-contato.component";
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,
-    RouterOutlet,
-    ContainerComponent,
-    CabecalhoComponent,
-    SeparadorComponent,
-    ContatoComponent,
-    FormsModule,
-    FormularioContatoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormularioContatoComponent,
+    ListaContatosComponent]
 })
 export class AppComponent {
-  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
-  contatos: Contato[] = agenda;
 
-  filtroPorTexto: string = ''
-
-  filtarContatosPorTexto(): Contato[]{
-    if(!this.filtroPorTexto){
-      return this.contatos
-    }
-
-    return this.contatos.filter(contato =>{
-       return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
-    })
-  }
-
-  filtrarContatoPorLetraInicial(letra: string): Contato[] {
-    return this.filtarContatosPorTexto().filter(contato => {
-      return contato.nome.toLowerCase().startsWith(letra)
-    })
-  }
 
 }
---
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { ContatoComponent } from '../../componentes/contato/contato.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from '../../agenda.json'
+
+@Component({
+  selector: 'app-lista-contatos',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+    FormularioContatoComponent],
+  templateUrl: './lista-contatos.component.html',
+  styleUrl: './lista-contatos.component.css'
+})
+export class ListaContatosComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = ''
+
+  filtarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos
+    }
+
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
+    })
+  }
+
+  filtrarContatoPorLetraInicial(letra: string): Contato[] {
+    return this.filtarContatosPorTexto().filter(contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    })
+  }
+}
