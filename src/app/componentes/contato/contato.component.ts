dd4e4130266240e32ffe86c23d93615ba26d4f8a
Matheus Koyama <koyamaCRS@gmail.com>
2024-06-25 15:54:47 -0300

    - Ajustando a rota pra o perfil-contato

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
@@ -11,4 +16,5 @@ export class ContatoComponent {
 
   @Input() nome: string = ''
   @Input() telefone: string = ''
+  @Input() id?:number;
 }
